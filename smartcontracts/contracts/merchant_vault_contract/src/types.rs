479cfb9ec734d9797820bd74df2c646d57a6e7f6
Nemenwq <chloemikka@gmail.com>
2026-06-13 13:58:49 +0100

    feat: add MerchantBalance and WithdrawalRequest structs

--- smartcontracts/contracts/merchant_vault_contract/src/types.rs
+++ smartcontracts/contracts/merchant_vault_contract/src/types.rs
@@ -7,6 +7,22 @@ pub enum DataKey {
     MerchantBalance(Address),
 }
 
+#[contracttype]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct MerchantBalance {
+    pub merchant_address: Address,
+    pub usdc_balance: i128,
+    pub last_updated: u64,
+}
+
+#[contracttype]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct WithdrawalRequest {
+    pub amount: i128,
+    pub destination: Address,
+    pub timestamp: u64,
+}
+
 #[contracterror]
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum Error {
