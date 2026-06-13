b46521b7800635bd7de6b969176e35bc4fdf3a79
Nemenwq <chloemikka@gmail.com>
2026-06-13 14:03:56 +0100

    feat: update storage.rs to use MerchantBalance struct

--- smartcontracts/contracts/merchant_vault_contract/src/storage.rs
+++ smartcontracts/contracts/merchant_vault_contract/src/storage.rs
@@ -1,5 +1,5 @@
 use soroban_sdk::{Env, Address};
-use crate::types::DataKey;
+use crate::types::{DataKey, MerchantBalance};
 
 pub fn get_admin(env: &Env) -> Option<Address> {
     env.storage().instance().get::<DataKey, Address>(&DataKey::VaultAdmin)
@@ -9,15 +9,14 @@ pub fn set_admin(env: &Env, admin: &Address) {
     env.storage().instance().set::<DataKey, Address>(&DataKey::VaultAdmin, admin);
 }
 
-pub fn get_merchant_balance(env: &Env, merchant: &Address) -> i128 {
+pub fn get_merchant_record(env: &Env, merchant: &Address) -> Option<MerchantBalance> {
     env.storage()
         .instance()
-        .get::<DataKey, i128>(&DataKey::MerchantBalance(merchant.clone()))
-        .unwrap_or(0)
+        .get::<DataKey, MerchantBalance>(&DataKey::MerchantBalance(merchant.clone()))
 }
 
-pub fn set_merchant_balance(env: &Env, merchant: &Address, balance: i128) {
+pub fn set_merchant_record(env: &Env, merchant: &Address, record: &MerchantBalance) {
     env.storage()
         .instance()
-        .set::<DataKey, i128>(&DataKey::MerchantBalance(merchant.clone()), &balance);
+        .set::<DataKey, MerchantBalance>(&DataKey::MerchantBalance(merchant.clone()), record);
 }
